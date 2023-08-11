cbb8a39fc2259756369fd1d2410ca4c3780f1c68
Mahmoud Korim Kandel <69222454+mahmoudkandel980@users.noreply.github.com>
2023-08-11 03:52:02 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
- * main - main block
+ * main - Entry point
  *
- * Description: 'Write a C program'
- * @parameter: void
- * 
- * Return : Always 0 (success)
+ * description : C program that prints put function
+ *
+ * Return : always 0 (success)
+ *
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
