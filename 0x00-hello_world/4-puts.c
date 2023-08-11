708ed42423448a176b7b1bb100a5f26b0d969489
Mahmoud Korim Kandel <69222454+mahmoudkandel980@users.noreply.github.com>
2023-08-11 03:30:43 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - main block
- * Return: always will be 0 (success)
- */
+ * main - Entry point
+ * Return : always will be 0 (success)
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
