56ec39eb26b514d28ac462be3398ba0183022612
Mahmoud Korim Kandel <69222454+mahmoudkandel980@users.noreply.github.com>
2023-08-12 03:18:08 +0300

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Main Entry
+* Return: 0 (success)
+*/
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 100; i++)
+		putchar(number + '0');
+	putchar('\n');
+	return (0);
+}
