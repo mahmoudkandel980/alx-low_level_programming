e1ebd2439d7fd32078a12889c6db1456fad35af2
Mahmoud Korim Kandel <69222454+mahmoudkandel980@users.noreply.github.com>
2023-08-12 03:34:51 +0300

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+/**
+* main - Main Entry
+* Return: 0 (success)
+*/
+int main(void)
+{
+	int number;
+
+	for (number = 48; number < 58; number++)
+		putchar(n);
+		if (n != 57)
+			putchar(',');
+			putchar(' ');
+	putchar('\n');
+	return (0);
+}
