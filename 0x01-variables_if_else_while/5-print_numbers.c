44c62f3c62443604c0d7ac8e03e2bfb7e0d26b2e
Mahmoud Korim Kandel <69222454+mahmoudkandel980@users.noreply.github.com>
2023-08-12 03:05:27 +0300

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Main Enty
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+		printf("%d", c);
+	printf("\c");
+	return (0);
+}
