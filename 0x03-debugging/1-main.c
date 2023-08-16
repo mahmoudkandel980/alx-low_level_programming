729f28cbd76501f148cd1c95a45f9755b593554e
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-08-16 10:45:17 -0700

    1. Like, comment, subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+* main - function 
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+	printf("Infinite loop incoming :(\n");
+	i = 0;
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}
