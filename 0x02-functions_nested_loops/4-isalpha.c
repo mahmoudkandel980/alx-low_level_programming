ca50fe880366144a4d126f0c8b929936607566e8
Mahmoud Korim Kandel <69222454+mahmoudkandel980@users.noreply.github.com>
2023-08-16 03:32:39 +0300

    4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,10 +6,18 @@
  * Return: 1 if character lowercase or uppercars, otherwise will be 0
 */
 
-int _isalpha(int var)
+int _isalpha(int c)
 {
-	if (var >= 97 && var <= 122 &&
-	    var >= 65 && var >= 90)
+	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
-	return (0);
+	}
+	else if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
