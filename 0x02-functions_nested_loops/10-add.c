ef406ea10f477b52e4f360626639357e1cf3c319
Mahmoud Korim Kandel <69222454+mahmoudkandel980@users.noreply.github.com>
2023-08-16 04:03:53 +0300

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* add - function
+* @firstVar: is intger
+* @secondVar: is intger
+* Return: sum
+*/
+
+int add(int firstVar, int secondVar)
+{
+	return (firstVar + secondVar);
+}
