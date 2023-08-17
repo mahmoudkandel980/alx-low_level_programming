0154c0b3346df869ed12df13e20bc998eb0f0a20
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-08-17 14:05:51 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *_isupper - is check if letters are uppercase or not.
+ *@c: is input.
+ *Return: 1 if c uppercase others willb be 0
+ */
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
