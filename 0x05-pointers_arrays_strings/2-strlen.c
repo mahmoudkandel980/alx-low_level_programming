62250b944678a4b902030311e74035e85428cb50
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-08-21 14:00:02 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - function
+ * @s: is an int
+ * Return: Alawys 0 (success)
+ */
+
+int _strlen(char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != '\0'; ++x)
+	{
+		continue;
+	}
+
+	return (x);
+}
