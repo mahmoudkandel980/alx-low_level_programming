78154150a9c9fa64a1b252dab83f23a46ab87336
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-08-21 14:42:26 -0700

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - function
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+    char *str;
+
+    str = "0123456789";
+
+    puts2(str);
+    
+    return (0);
+}
