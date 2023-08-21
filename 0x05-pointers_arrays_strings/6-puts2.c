3ce3f50aedcddc16a1195595efbc4b182df9bdbf
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-08-21 14:48:29 -0700

    6. Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,27 +1,6 @@
 #include "main.h"
 
 /**
-* _strlen - function
-* @s: is a char
-* Return: x
-**/
-
-int _strlen(char *s)
-{
-	int x;
-	int y = 0;
-
-	while (s[y] != '\0')
-	{
-		y++;
-	}
-
-	x = y;
-	
-	return (y);
-}
-
-/**
  * puts2 - function
  * @str: is a char
  * Return: Always 0 (success)
