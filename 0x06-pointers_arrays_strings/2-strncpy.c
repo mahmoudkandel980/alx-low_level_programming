6bf4386d53a54c285055f56dd710968cb402b8c1
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-08-23 16:00:19 -0700

    2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* *_strncpy - function
+* @dest: string
+* @src: string
+* @n: numberr
+* Return: string
+**/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x = 0;
+
+	while (src[x] != '\0')
+	{
+		if (x < n)
+			dest[x] = src[x];
+
+		i++;
+	}
+
+	while (x < n)
+	{
+		dest[x] = '\0';
+		x++;
+	}
+	return (dest);
+}
