c922e1293ecc6414409fdef955764046ae69aa8e
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-08-23 16:10:36 -0700

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - functon
+ * @s1: string
+ * @s2: string
+ * Return: 0 or b
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int x = 0;
+	int y = 0;
+
+	while (s1[x] != '\0' && y == 0)
+		y = s1[x] - s2[x];
+		x++;
+
+	return (y);
+}
