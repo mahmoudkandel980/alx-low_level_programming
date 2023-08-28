80037f9fdd8076075ea7e88fe2d8761c81b4447a
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-08-28 15:56:22 -0700

    4. strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,21 +1,23 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* _strpbrk - function
+* @s: character
+* @accept: string
+* Return: pointer
+**/
 
-int main(void)
+char *_strpbrk(char *s, char *accept)
 {
-    char *x = "hello, world";
-    char *y = "world";
-    char *z;
+	int x, y;
 
-    z = _strpbrk(x, y);
-
-    printf("%s\n", z);
-
-    return (0);
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; accept[y] != '\0'; y++)
+		{
+			if (s[x] == accept[y])
+				return (s + x);
+		}
+	}
+	return (0);
 }
