ce2a49c1cf466243e12efbed3b62dadd20628259
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-09-26 08:34:25 -0700

    0. Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - function list of elements
+ * @h: head of list
+ * Return: nodes of numbers
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t node = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		node++;
+	}
+	return (node);
+}
