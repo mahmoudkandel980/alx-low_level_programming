18d169cee5ced530ff0fc29a1c267dcc4d411530
Mahmoud Korim Kandel <69222454+mahmoudkandel980@users.noreply.github.com>
2023-10-02 23:55:54 +0300

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	int numberOfLetter;
 	int rwr;
 
-	if (!filename == NULL)
+	if (filename == NULL)
     {
 		return (-1);
     }
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
     }
 
-	if (!text_content == NULL)
+	if (text_content == NULL)
     {
 		text_content = "";
     }
