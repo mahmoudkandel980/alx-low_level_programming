ecee2d2cfc6313455b3ff1810dcb909e9ef37206
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-11-10 11:46:12 -0800

    1. List length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Fun Counts the number of elements in list.
+ * @h: The list head
+ *
+ * Return: The elements number
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t n = 0;
+
+	while (h !== NULL)
+	{
+		n++;
+		h = h->next;
+	}
+
+	return (n);
+}
