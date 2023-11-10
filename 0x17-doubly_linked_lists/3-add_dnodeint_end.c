a6db0316c9c53900244dcddfb75810920415aace
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-11-10 11:59:10 -0800

    #include lists.h

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - Fun adds a new node at the end of lisr
+ * @head: A head of list
+ * @n: int
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new
+	dlistint_t *last;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
+
+	last = *head;
+	while (last->next)
+	{
+		last = last->next;
+	}
+	
+	last->next = new;
+	new->prev = last;
+
+	return (new);
+}
