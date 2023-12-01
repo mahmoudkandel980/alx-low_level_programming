51c7c2d85787ecfb565922e376f9268e6ba16501
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-12-01 11:56:17 -0800

    3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,36 +1,35 @@
 #include "hash_tables.h"
+
 /**
- *hash_table_set - adds an element to the hash table
- *@ht: Pointer to hash
- *@key: Pointer to the key
- *@value: value
- *Return: success (1), otherwise 0
+ * hash_table_set - adds an element to the hash table
+ * @ht: Pointer to hash
+ * @key: Pointer to the key
+ * @value: value
+ * Return: success (1), otherwise 0
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
-{
-unsigned long i, tabelSize;
-hash_node_t *newNode;
+int hash_table_set(hash_table_t *ht, const char *key, const char *value) {
+    unsigned long i, tableSize;
+    hash_node_t *newNode;
 
-if (ht  == NULL || key == NULL || value == NULL)
-return (0);
+    if (ht == NULL || key == NULL || value == NULL)
+        return (0);
 
-tabelSize = ht->tabelSize;
-i = key_index((const unsigned char *)key, tabelSize);
+    tableSize = ht->tableSize;
+    i = key_index((const unsigned char *)key, tableSize);
 
-if (ht->array[i] != NULL && strcmp(ht->array[i]->key, key) == 0)
-{
-ht->array[i]->value = strdup(value);
-return (1);
-}
+    if (ht->array[i] != NULL && strcmp(ht->array[i]->key, key) == 0) {
+        ht->array[i]->value = strdup(value);
+        return (1);
+    }
 
-newNode = malloc(sizeof(hash_node_t));
+    newNode = malloc(sizeof(hash_node_t));
+    if (newNode == NULL)
+        return (0);
 
-if (newNode == NULL)
-return (0);
+    newNode->key = strdup(key);
+    newNode->value = strdup(value);
+    newNode->next = ht->array[i];
+    ht->array[i] = newNode;
 
-newNode->key = strdup(key);
-newNode->value = strdup(value);
-newNode->next = ht->array[i];
-ht->array[i] = newNode;
-return (1);
+    return (1);
 }
