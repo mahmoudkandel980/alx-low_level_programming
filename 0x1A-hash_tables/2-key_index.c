33cffc4bebe9a7544142e4e5a04f48d1ad289249
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-12-01 11:49:51 -0800

    2. key -> index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Generates key
+ * @key: Key pointer
+ * @size: tabel size
+ * Return: index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+    if (key == NULL || size == 0)
+    {
+		return (0);
+    }
+	return (hash_djb2(key) % size);
+}
